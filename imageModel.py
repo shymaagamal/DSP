60e5feacb41b756cb4030662a6ed6498597bc020
Shaimaa Gamal <shymaagamal666@gmail.com>
2020-04-13 10:12:27 +0200

    modified

--- imageModel.py
+++ imageModel.py
@@ -1,3 +1,5 @@
+## This is the abstract class that the students should implement
+
 from modesEnum import Modes
 import numpy as np
 
@@ -6,119 +8,99 @@ import numpy as np
 from modesEnum import Modes
 
 import cv2 as cv
-from PyQt5.QtGui import QPixmap
+from PyQt5.QtGui import QPixmap ,QImage
 from PyQt5 import QtCore, QtGui, QtWidgets
 import numpy as np
 from matplotlib import pyplot as plt
+import  logging 
+
+logger=logging.getLogger(__name__)
+fileHandler=logging.FileHandler('ApplicationWindow.log')
+Formatter=logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+streamHandler=logging.StreamHandler()
+streamHandler.setFormatter(Formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(fileHandler)
+logger.addHandler(streamHandler)
+fileHandler.setFormatter(Formatter)
+
 class ImageModel():
 
     """
     A class that represents the ImageModel"
     """
-
+    
     def __init__(self):
 
         pass
-
+    
     def __init__(self, imgPath: str):
         self.imgPath = imgPath
         ###
         # ALL the following properties should be assigned correctly after reading imgPath 
         ###
-
-        self.imgByte = None
-        self.dft = None
-        self.real = None
-        self.imaginary = None
-        self.magnitude = None
-        self.phase = None
-        self.invmagnitude=None
-        self.invphase=None
-        self.unimagnitude=None
-        self.uniphase=None
-    def image_Display(self,winDisplay):
-
-        self.imgByte=QPixmap(self.imgPath)
-        winDisplay.setScaledContents(True)
-        winDisplay.setPixmap(self.imgByte)
-    def modify_Component(self): 
-        self.imgPath=QtGui.QImage(self.imgPath, self.imgPath.shape[0],self.imgPath.shape[1],QtGui.QImage.Format_Grayscale8)
-    def check_Size(self):
-        self.imgByte=cv.imread(self.imgPath)    
-        return  self.imgByte
-    def fourier_Transform(self):
-        self.imgByte = cv.imread(self.imgPath)
+        
+        self.imgByte = cv.imread(self.imgPath,cv.IMREAD_GRAYSCALE)
+        self.imgByte = np.asarray( self.imgByte, dtype="int32"  )
         self.dft = np.fft.fft2(self.imgByte)
         self.dft = np.fft.fftshift(self.dft) 
-        return self.dft
-    def magnitude_Component(self):
-        self.dft = self.fourier_Transform()
+
         self.magnitude =np.abs(self.dft)
-        return self.magnitude
-    def phase_Component(self):
-        self.dft = self.fourier_Transform()
+
+        self.Real=np.real(self.dft)
+        self.real=np.array(self.Real).reshape(-1,2).astype(np.int32)
+
+        self.Imaginary = np.imag(self.dft)
+        self.imaginary=np.array(self.Imaginary)
+        
         self.phase = np.angle(self.dft)
-        return self.phase
-    def real_Component(self):
-        self.dft = self.fourier_Transform()
-        self.real=np.real(self.dft)
-        self.real=np.array(self.real).reshape(-1,2).astype(np.int32)
-        return self.real 
-    def imaginary_Component(self):
-        self.dft = self.fourier_Transform()
-        self.imaginary = np.imag(self.dft)
-        self.imaginary=np.array(self.imaginary)
-        return self.imaginary 
-    def uni_Phase_Component(self):
-        self.dft = self.fourier_Transform()
-        self.uniphase = np.angle(self.dft)
-        self.uniphase=np.zeros(self.uniphase.shape,dtype=None ,order='C')
-        return self.uniphase
-    def uni_Magnitude_Component(self):
-        self.dft = self.fourier_Transform()
-        self.unimagnitude = np.abs(self.dft)
-        self.unimagnitude=np.ones(self.unimagnitude.shape,dtype=None ,order='C')
-        return self.unimagnitude
-    def invers_Magnitude_Component(self):
-        self.dft = self.magnitude_Component()
-        self.f_ishift = np.fft.ifftshift(self.dft)
-        self.invmagnitude = np.fft.ifft2(self.f_ishift)
-        return self.invmagnitude
-    def invers_Phase_Component(self):
-        self.dft = self.phase_Component()
-        self.f_ishift = np.fft.ifftshift(self.dft)
-        self.invphase = np.fft.ifft2(self.f_ishift)
-        return self.invphase
+        self.invmagnitude=None
+        self.invphase=None
+        self.unimagnitude=np.ones(self.magnitude.shape,dtype=None ,order='C')
+        self.uniphase=np.zeros(self.phase.shape,dtype=None ,order='C')
+        self.invmagnitude = np.fft.ifft2(np.fft.ifftshift( self.magnitude))
+        self.invphase = np.fft.ifft2(np.fft.ifftshift( self.phase))
+    def image_Display(self,winDisplay):
+        try:
+            self.imgByte.shape
+            print(self.imgByte.shape)
+            self.image=QImage(self.imgByte, self.imgByte.shape[0],self.imgByte.shape[1],QImage.Format_Grayscale8)
+            self.image=QPixmap.fromImage(self.image)
+            winDisplay.setScaledContents(True)
+            winDisplay.setPixmap(self.image)
+            
+        except AttributeError:
+            print("shape not found")
+        logger.debug('Displayed Image')
+
     def mix(self, imageToBeMixed: 'ImageModel', magnitudeOrRealRatio: float, phaesOrImaginaryRatio: float, mode: 'Modes') -> np.ndarray:
         ###
         #a function that takes ImageModel object mag ratio, phase ration and
         #return the magnitude of ifft of the mix
         #3return type ---> 2D numpy array
         #please Add whatever functions realted to the image data in this file
-
+        
         ###
         # implement this function
         ###
         if mode==Modes.PhaseAndMagnitudeMode.value:
-
+            
             self.magnitudeImg2=imageToBeMixed.magnitude_Component()
             self.phaseImg2=imageToBeMixed.phase_Component()
-            #self.mixMag=np.add(self.magnitude_Component() * magnitudeOrRealRatio,self.magnitudeImg2*(1- magnitudeOrRealRatio))
-            self.mixMag=self.magnitude_Component() * magnitudeOrRealRatio+self.magnitudeImg2*(1- magnitudeOrRealRatio)
-            #self.mixPhase=np.add((1-phaesOrImaginaryRatio)* self.phase_Component() ,magnitudeOrRealRatio* self.phaseImg2)
-            self.mixPhase=(1-phaesOrImaginaryRatio)* self.phase_Component() +magnitudeOrRealRatio* self.phaseImg2
-            #self.mix=np.multiply(self.mixMag,np.exp(1j*self.mixPhase)) 
+            self.mixMag=self.magnitude_Component() * magnitudeOrRealRatio + self.magnitudeImg2*(1- magnitudeOrRealRatio)
+            self.mixPhase=(1-phaesOrImaginaryRatio)* self.phase_Component() + phaesOrImaginaryRatio* self.phaseImg2
             self.mix=self.mixMag*np.exp(1j*self.mixPhase)
             self.mix=np.fft.ifft2(self.mix)
+            logger.debug('mix between Phase and magnitude')
         elif mode==Modes.realAndImaginaryMode.value:
-            self.realImg1=self.real_Component()
-            self.imagImg1=self.imaginary_Component()
             self.realImg2=imageToBeMixed.real_Component()
             self.imagImg2=imageToBeMixed.imaginary_Component()
-            self.mixReal=np.add(self.magnitude * magnitudeOrRealRatio,self.magnitudeImg2*(1- magnitudeOrRealRatio))
-            self.mixImag=np.add((1-phaesOrImaginaryRatio)* self.phase ,magnitudeOrRealRatio* self.phaseImg2)
-            self.mix=np.multiply(self.mixMag,np.exp(self.mixPhase)) 
+            self.mixReal=self.real_Component() * magnitudeOrRealRatio + self.realImg2*(1- magnitudeOrRealRatio)
+            self.mixImag=(1-phaesOrImaginaryRatio)* self.imaginary_Component() + phaesOrImaginaryRatio* self.imagImg2
+            self.mix=self.mixReal +self.mixImag
+            
             self.mix=np.fft.ifft2(self.mix)
-            self.mix=np.abs(self.mix)
-
+            self.mix=np.array(self.mix).reshape(-1,2)
+            logger.debug('mix between Real and Imaginary')
         return self.mix
+     
--- main.py
+++ main.py
@@ -3,15 +3,26 @@ from Qt import Ui_MainWindow
 import sys
 import cv2 as cv
 import numpy as np
-from PyQt5.QtGui import QPixmap
 
+from PyQt5.QtGui import QPixmap ,QImage
 from PyQt5 import QtCore, QtGui, QtWidgets
 from modesEnum import Modes
 from matplotlib import pyplot as plt
 from imageModel import ImageModel
-
+import imageModel
 from PIL import Image
 from numpy import asarray
+import  logging 
+
+logger=logging.getLogger(__name__)
+fileHandler=logging.FileHandler('ApplicationWindow.log')
+Formatter=logging.Formatter('%(levelname)s:%(name)s:%(message)s')
+streamHandler=logging.StreamHandler()
+streamHandler.setFormatter(Formatter)
+logger.setLevel(logging.DEBUG)
+logger.addHandler(fileHandler)
+logger.addHandler(streamHandler)
+fileHandler.setFormatter(Formatter)
 
 
 class ApplicationWindow(QtWidgets.QMainWindow):
@@ -23,26 +34,29 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         self.ImagButton2=0
         self.sizeFlag1=0
         self.sizeFlag2=0
+        
+        
         self.ui.DisplayIm_1.clicked.connect(lambda:self.whichBtn(self.ui.DisplayIm_1))
         self.ui.DisplayIm_1.clicked.connect(self.open_Image)
         self.ui.DisplayIm_2.clicked.connect(lambda:self.whichBtn(self.ui.DisplayIm_2))
         self.ui.DisplayIm_2.clicked.connect(self.open_Image)
-        self.ui.comboBox_1.activated.connect(lambda:self.which_Com_Box(self.ui.comboBox_1))
-        self.ui.comboBox_1.activated.connect(self.display_DFT)
-        self.ui.comboBox_2.activated.connect(lambda:self.which_Com_Box(self.ui.comboBox_2))
-        self.ui.comboBox_2.activated.connect(self.display_DFT)
+        self.ui.comboBox_1.activated.connect(lambda:self.display_DFT(self.ui.comboBox_1,self.img1,self.ui.Image_3))
+        self.ui.comboBox_2.activated.connect(lambda:self.display_DFT(self.ui.comboBox_2,self.img2,self.ui.Image_4))
         self.ui.horizontalSlider.valueChanged.connect(lambda:self.slider_Value(self.ui.horizontalSlider))
         self.ui.horizontalSlider_2.valueChanged.connect(lambda:self.slider_Value(self.ui.horizontalSlider_2))
-        self.ui.radioButton.toggled.connect(self.radio_Button)
-        self.ui.radioButton_2.toggled.connect(self.radio_Button)
-        self.ui.comboBox_3.activated.connect(self.Mode)
+        self.ui.radioButton.toggled.connect(self.option)
+        self.ui.radioButton_2.toggled.connect(self.option)
+        self.ui.comboBox_3.activated.connect(self.option)
         self.ui.comboBox_5.activated.connect(self.output)
+    
     def whichBtn(self,b):
         if b==self.ui.DisplayIm_1:
+             logger.debug('Clicked on DisplayIm_1 Button')
              self.ImagButton1=1
              self.sizeFlag1=1
              self.ImagButton2=0
         elif b==self.ui.DisplayIm_2:
+            logger.debug('Clicked on DisplayIm_2 Button')
             self.ImagButton1=0
             self.sizeFlag2=1
             self.ImagButton2=1
@@ -53,126 +67,104 @@ class ApplicationWindow(QtWidgets.QMainWindow):
         msgBox.setText(message)
         msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok)
         msgBox.exec_()
-
+    
     def open_Image(self):
-        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Single File', QtCore.QDir.rootPath() , '*.jpg')
+        fileName, _ = QtWidgets.QFileDialog.getOpenFileName(self, 'Single File' , '*.jpg')
         if self.ImagButton1==1:
-            self.imgByte1 = cv.imread(fileName)
             self.img1=ImageModel(fileName)
+            self.sizeImg1=self.img1.imgByte.shape
             self.img1.image_Display(self.ui.Image_1)
             if self.sizeFlag2==1:
-                self.sizeImg1=self.img1.check_Size()
-                self.sizeImg2=self.img2.check_Size()
-                if self.sizeImg1.shape !=self.sizeImg2.shape:
-                     self.showMessageBox('Warning','Two Images have not the same size' )
+                if self.sizeImg1!=self.sizeImg2:
+                    logger.debug('Two Images have not the same size')
+                    self.showMessageBox('Warning','Two Images have not the same size' )               
         elif self.ImagButton2==1:
-            self.imgByte2 = cv.imread(fileName)
+            
             self.img2=ImageModel(fileName)
             self.img2.image_Display(self.ui.Image_2)
             if self.sizeFlag1==1:
-                self.sizeImg1=self.img1.check_Size()
-                self.sizeImg2=self.img2.check_Size()
-                if self.sizeImg1.shape !=self.sizeImg2.shape:
+                self.sizeImg2=self.img2.imgByte.shape
+                if self.sizeImg1 !=self.sizeImg2:
+                    logger.debug('Two Images have not the same size')
                     self.showMessageBox('Warning','Two Images have not the same size')
-    def which_Com_Box(self,com):
-        if com==self.ui.comboBox_1:
-            self.comFlag1=1
-            self.comFlag2=0
-        elif com==self.ui.comboBox_2:
-            self.comFlag2=1  
-            self.comFlag1=0  
-    def display_DFT(self):
-            if self.comFlag1==1:
-                self.comImg=self.ui.comboBox_1.currentText()
-                if self.comImg ==Modes.magnitudeFFT.value:
-                    self.spectrum=self.img1.magnitude_Component()
-                elif self.comImg ==Modes.phaseFFT.value:
-                    self.spectrum=self.img1.phase_Component()
-                elif self.comImg ==Modes.realFFT.value:
-                    self.spectrum=self.img1.real_Component()
-                elif self.comImg ==Modes.imaginaryFFT.value:
-                        self.spectrum=self.img1.imaginary_Component()
-                elif self.comImg ==Modes.inverseMagnitudeFFT.value:
-                        self.spectrum=self.img1.invers_Magnitude_Component()
-                elif self.comImg ==Modes.inversePhaseFFT.value:
-                        self.spectrum=self.img1.invers_Phase_Component()
-                elif self.comImg ==Modes.uniMagnitude.value:
-                        self.spectrum=self.img1.uni_Magnitude_Component()
-                elif self.comImg ==Modes.uniPhase.value:
-                        self.spectrum=self.img1.uni_Phase_Component()
+                    
+    def display_DFT(self,ComboBox,Imge,QLable):
+        self.comImg=ComboBox.currentText()
+        if self.comImg ==Modes.magnitudeFFT.value:
+            logger.debug('Chose FT Magnitude component Image')
+            self.spectrum=Imge.magnitude
+            logger.debug('returned magnitude_Component_Imag1')
+        elif self.comImg ==Modes.phaseFFT.value:
+            logger.debug('Chose FT Phase component Image')
+            self.spectrum=Imge.phase
+            logger.debug('returned phase_Component_Imag1')
+        elif self.comImg ==Modes.realFFT.value:
+            logger.debug('Chose FT Real component Image')
+            self.spectrum=Imge.real
+            logger.debug('returned real_Component_Imag1')
+        elif self.comImg ==Modes.imaginaryFFT.value:
+            logger.debug('Chose FT Imaginary component Image')
+            self.spectrum=Imge.imaginary
+            logger.debug('returned imaginary_Component_Imag1')
+        elif self.comImg ==Modes.inverseMagnitudeFFT.value:
+            logger.debug('Chose Inverse FT Magnitude component Image')
+            self.spectrum=Imge.invmagnitude
+            logger.debug('returned invers_Magnitude_Component_Imag1')
+        elif self.comImg ==Modes.inversePhaseFFT.value:
+            logger.debug('Chose Inverse FT Phase component Image')
+            self.spectrum=Imge.invphase
+            logger.debug('returned invers_Phase_Component_Imag1')
+        elif self.comImg ==Modes.uniMagnitude.value:
+            logger.debug('Chose UniForm Magnitude component Image')
+            self.spectrum=Imge.unimagnitude
+            logger.debug('returned uni_Magnitude_Component_Imag1')
+        elif self.comImg ==Modes.uniPhase.value:
+            logger.debug('Chose UniForm Phase component Image')
+            self.spectrum=Imge.uniphase
+            logger.debug('returned uni_Phase_Component_Imag1')
 
-
-                self.pix=ImageModel(self.spectrum)
-                self.pix. modify_Component()
-                self.pix.image_Display(self.ui.Image_3)
-
-            elif self.comFlag2 == 1:
-                self.comImg=self.ui.comboBox_2.currentText()
-                if self.comImg ==Modes.magnitudeFFT.value:
-                    self.spectrum=self.img2.magnitude_Component()
-                elif self.comImg ==Modes.phaseFFT.value:
-                    self.spectrum=self.img2.phase_Component()
-                elif self.comImg ==Modes.realFFT.value:
-                    self.spectrum=self.img2.real_Component()
-                elif self.comImg ==Modes.imaginaryFFT.value:
-                        self.spectrum=self.img2.imaginary_Component()
-                elif self.comImg ==Modes.inverseMagnitudeFFT.value:
-                        self.spectrum=self.img2.invers_Magnitude_Component()
-                elif self.comImg ==Modes.inversePhaseFFT.value:
-                        self.spectrum=self.img2.invers_Phase_Component()
-                elif self.comImg ==Modes.uniMagnitude.value:
-                        self.spectrum=self.img2.uni_Magnitude_Component()
-                elif self.comImg ==Modes.uniPhase.value:
-                        self.spectrum=self.img2.uni_Phase_Component()
-
-                self.pix=ImageModel(self.spectrum)
-                self.pix. modify_Component()
-                self.pix.image_Display(self.ui.Image_4)
-
+        self.image=QImage(self.spectrum, self.spectrum.shape[0],self.spectrum.shape[1],QImage.Format_Grayscale8)
+        self.image=QPixmap.fromImage(self.image)
+        QLable.setScaledContents(True)
+        QLable.setPixmap(self.image)
+        logger.debug('Plot Component')
     def slider_Value(self,slider):
         if slider==self.ui.horizontalSlider:
             self.magnitudeOrRealRatio=float(self.ui.horizontalSlider.value()/100) 
+            logger.debug('Slider_Value:{}'.format(self.magnitudeOrRealRatio))
         elif slider==self.ui.horizontalSlider_2:
             self.phaesOrImaginaryRatio=float(self.ui.horizontalSlider_2.value()/100) 
-
-    def radio_Button(self):
+            logger.debug('Slider_Value:{}'.format(self.phaesOrImaginaryRatio))
+    def option(self):
+        self.comImg=self.ui.comboBox_3.currentText() 
         if self.ui.radioButton.isChecked():
-            self.radioFlag1=1
-            self.radioFlag2=0
+           self.Mode(self.img1,self.img2,self.comImg)
         elif self.ui.radioButton_2.isChecked():
-            self.radioFlag1=0
-            self.radioFlag2=1   
+            self.Mode(self.img2,self.img1,self.comImg) 
+    def Mode(self,Img,ImgToBeMixed,mode ):
+            
+            if mode==Modes.PhaseAndMagnitudeMode.value:
+                logger.debug('Mix between Phase and Magnitde')
+                self.mix=Img.mix(ImgToBeMixed,self.magnitudeOrRealRatio,self.phaesOrImaginaryRatio,Modes.PhaseAndMagnitudeMode.value)               
+            elif  mode==Modes.realAndImaginaryMode.value:
+                self.mix=Img.mix(ImgToBeMixed,self.magnitudeOrRealRatio,self.phaesOrImaginaryRatio,Modes.realAndImaginaryMode.value)
+                logger.debug('Mix between Imaginary and Real')
 
-    def Mode(self):
-        self.comImg=self.ui.comboBox_3.currentText()
-        if self.radioFlag1==1:    
-            if self.comImg==Modes.PhaseAndMagnitudeMode.value:
-                self.mix=self.img1.mix(self.img2,self.magnitudeOrRealRatio,self.phaesOrImaginaryRatio,Modes.PhaseAndMagnitudeMode.value)
-            elif  self.comImg==Modes.realAndImaginaryMode.value:
-                self.mix=self.img1.mix(self.img2,self.magnitudeOrRealRatio,self.phaesOrImaginaryRatio,Modes.realAndImaginaryMode.value)
-
-        elif self.radioFlag2==1:
-            if self.comImg==Modes.PhaseAndMagnitudeMode.value:
-                self.mix=self.img2.mix(self.img1,self.magnitudeOrRealRatio,self.phaesOrImaginaryRatio,Modes.magnitudeAndPhase.value)
-            elif  self.comImg==Modes.realAndImaginaryMode.value:
-                self.mix=self.img2.mix(self.img1,self.magnitudeOrRealRatio,self.phaesOrImaginaryRatio,Modes.realAndImaginaryMode.value)
     def output(self):
         self.comImg=self.ui.comboBox_5.currentText() 
         self.pix=ImageModel(self.mix)
-        self.pix. modify_Component() 
         if self.comImg==Modes.outputDisplay1.value:
             self.pix.image_Display(self.ui.output_1) 
+            
         elif self.comImg==Modes.outputDisplay2.value :
             self.pix.image_Display(self.ui.output_2)
-
-
+        logger.debug('Display on Output')
+   
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     application = ApplicationWindow()
     application.show()
     app.exec_()
-
-
 if __name__ == "__main__":
-    main() 
+    main()
